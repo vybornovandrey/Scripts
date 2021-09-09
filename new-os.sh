14e255f05ead0fd239f62d6bd5fc889d958c9e66
vybornovandrey <62988949+vybornovandrey@users.noreply.github.com>
2021-09-09 16:07:08 +0300

    Update new-os.sh

--- new-os.sh
+++ new-os.sh
@@ -2,7 +2,6 @@
 echo "*******************************"
 echo "*  Begin update new CentOS 7  *"
 echo "*******************************"
-sleep 5
 
 yum install -y epel-release
 yum update -y
@@ -21,26 +20,26 @@ yum install -y \
     tree \
     vim \
     wget \
-    yum-plugin-remove-with-leaves
-    yum-utils \
+    yum-plugin-remove-with-leaves \
+    yum-utils
 
 #Добавляем пользователя
 useradd andrey
 usermod -aG wheel andrey
 
+mkdir /home/andrey/.ssh && chown andrey:andrey /home/andrey/.ssh && chmod 700 /home/andrey/.ssh
+touch /home/andrey/.ssh/authorized_keys && chown andrey:andrey /home/andrey/.ssh/authorized_keys && chmod 600 /home/andrey/.ssh/authorized_keys
+
+# add ssh key for user andrey
 cat > /home/andrey/.ssh/authorized_keys <<EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAyH4Ozx/OsK7lxLv+HQnX9Kh2inorKlUCTe8oXia+SCQHIHMnLKgydSjY/wLUDhTHPiSwX9WLXQYIRMRSCOq5ThOvRBNhXqXTEsAarYiYrtqt5YhiwVGZtLA7ZpR/g6tu07cqJY7SBwH1QkIP3wN8zWjdM9UjqdacA/13hVRRn/Li2P9BaWAG8FkfAf75j83WPL803plaMYWbDO54t/eLl7qLMdaKkmPx+IH7wOSjhlkzdt2LjDUbi2VWC9Xptvv47gABteXlrOXalZzVGsY8m9vkMJPQXTfaD9C6I5cvif7J/vYKmV8OKFgW/SSXbFGZoD1vLExwqaMC07MW3+yvxQ== anvy0321@WS-14493
 EOF
 
-chmod 700 /home/andrey/.ssh
-chmod 600 /home/andrey/.ssh/authorized_keys
-
+#Настраиваем (отключаем) firewall
 echo "*******************************"
 echo "*      Disable Firewall       *"
 echo "*******************************"
 sleep 5
-#Настраиваем (отключаем) firewall
-systemctl status firewalld
 systemctl stop firewalld
 systemctl disable firewalld
 
